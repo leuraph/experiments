b9661b8a1577abacbc0533be44fcf3f23795c31c
rleu <raphaelleu95@gmail.com>
2024-05-15 14:57:52 +0200

    chore: nicer plotting of mesh

--- variational_adaptivity/gauss_kick/plot_mesh.py
+++ variational_adaptivity/gauss_kick/plot_mesh.py
@@ -1,11 +1,17 @@
 import pickle
 import sys
 from pathlib import Path
-from p1afempy.mesh import show_mesh
-from variational_adaptivity.utils import plot_result
+import matplotlib.pyplot as plt
 
 
 def main() -> None:
+    plt.rcParams["mathtext.fontset"] = "cm"
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['axes.titlesize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+
     base_path = Path(sys.argv[1])
 
     path_to_coordinates = base_path / Path('coordinates.pkl')
@@ -16,7 +22,17 @@ def main() -> None:
     with open(path_to_elements, mode='rb') as file:
         elements = pickle.load(file=file)
 
-    show_mesh(coordinates=coordinates, elements=elements)
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'$x$-coordinate')
+    ax.set_ylabel(r'$y$-coordinate')
+
+    for element in elements:
+        r0, r1, r2 = coordinates[element, :]
+        ax.plot(
+            [r0[0], r1[0], r2[0], r0[0]],
+            [r0[1], r1[1], r2[1], r0[1]],
+            'black', linewidth=0.3)
+    plt.show()
 
 
 if __name__ == '__main__':
