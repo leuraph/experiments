7602e34b7e982ddaf00fd52241b5d852b5c67a59
rleu <raphaelleu95@gmail.com>
2024-05-16 18:21:08 +0200

    refactor: recycle configration-py in numerical experiments

--- variational_adaptivity/gauss_kick/experiment_03_order_2_shuffle.py
+++ variational_adaptivity/gauss_kick/experiment_03_order_2_shuffle.py
@@ -1,53 +1,14 @@
-"""
-This script considers the problem
--laplace u = f, on (0,1)^2
-u=0 on boundary of (0,1)^2
-
-where the exact solution is imposed to be
-u(x, y) = y(y-1)x(x-1) exp(-sigma_x(x-mu_x)^2 -sigma_y(y-mu_y)^2)
-f(x, y) = -laplace u
-
-Notes
------
-- the symbolic computations are outsourced to a jupyter notebook using `sympy`
-"""
 import numpy as np
 from p1afempy import io_helpers, refinement, solvers
 from p1afempy.mesh import get_element_to_neighbours
 from pathlib import Path
 from variational_adaptivity import algo_4_1, markers
 from utils import shuffle_elements
+from configuration import uD, f, ExperimentConfig
 import time
 import pickle
 import pandas as pd
 import argparse
-
-
-sigma_x = 100.
-sigma_y = 40.
-mu_x = 37./73
-mu_y = 41./73.
-
-N_REFINEMENTS = 9
-N_INITIAL_REFINEMENTS = 2
-
-
-def analytical(r: np.ndarray) -> np.ndarray:
-    x, y = r[:, 0], r[:, 1]
-    u = (x*y*(x - 1)*(y - 1)*np.exp(-sigma_x*(-mu_x + x)**2 - sigma_y*(-mu_y + y)**2))
-    return u
-
-
-def f(r: np.ndarray) -> float:
-    """returns -((d/dx)^2 + (d/dy)^2)analytical(x,y)"""
-    x, y = r[:, 0], r[:, 1]
-    laplace = (2*(x*(x - 1)*(2*sigma_y*y*(mu_y - y) + sigma_y*y*(y - 1)*(2*sigma_y*(mu_y - y)**2 - 1) + 2*sigma_y*(mu_y - y)*(y - 1) + 1) + y*(y - 1)*(2*sigma_x*x*(mu_x - x) + sigma_x*x*(x - 1)*(2*sigma_x*(mu_x - x)**2 - 1) + 2*sigma_x*(mu_x - x)*(x - 1) + 1))*np.exp(-sigma_x*(-mu_x + x)**2 - sigma_y*(-mu_y + y)**2))
-    return -laplace
-
-
-def uD(r: np.ndarray) -> np.ndarray:
-    """returns homogeneous boundary conditions"""
-    return np.zeros(r.shape[0])
 
 
 def main() -> None:
@@ -84,7 +45,7 @@ def main() -> None:
 
     # initial refinement
     # ------------------
-    n_initial_refinements = N_INITIAL_REFINEMENTS
+    n_initial_refinements = ExperimentConfig.N_INITIAL_REFINEMENTS
     for _ in range(n_initial_refinements):
         marked = np.arange(elements.shape[0])
         coordinates, elements, boundaries, _ = \
@@ -119,7 +80,7 @@ def main() -> None:
     # variational adaptivity
     # ------------------------------------------------
 
-    for n_refinement in np.arange(1, N_REFINEMENTS + 1):
+    for n_refinement in np.arange(1, ExperimentConfig.N_REFINEMENTS + 1):
         start_time = time.process_time_ns()
 
         # compute all local energy gains
