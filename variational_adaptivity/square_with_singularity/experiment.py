415a8ea0e5537504bfab0a73ab19c27af492f630
rleu <raphaelleu95@gmail.com>
2024-05-27 17:52:30 +0200

    added exp: square with singularity

---
+++ variational_adaptivity/square_with_singularity/experiment.py
@@ -0,0 +1,188 @@
+"""
+This script considers the problem
+-laplace u = f, on (0,1)^2
+u=0 on boundary of (0,1)^2
+
+where an explicit expression for the analytic solution u is unavailable.
+However, the energy norm may be represented as an
+eigenfunction expansion and yields that [1]
+
+|| u ||_a^2 = || nabla u ||_L2^2 = 0.035144253738788451...
+
+References
+----------
+[1] 10.48550/arXiv.2311.13255
+"""
+import numpy as np
+from p1afempy import io_helpers, refinement, solvers
+from p1afempy.mesh import get_element_to_neighbours
+from pathlib import Path
+from variational_adaptivity import algo_4_1, markers
+import time
+import pickle
+import pandas as pd
+import argparse
+
+N_REFINEMENTS = 10
+N_INITIAL_REFINEMENTS = 2
+
+
+def f(r: np.ndarray) -> float:
+    """returns -((d/dx)^2 + (d/dy)^2)analytical(x,y)"""
+    n_coordinates = r.shape[0]
+    return np.ones(n_coordinates)
+
+
+def uD(r: np.ndarray) -> np.ndarray:
+    """returns homogeneous boundary conditions"""
+    return np.zeros(r.shape[0])
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--theta", type=float, required=True,
+                        help="value of theta used in the Dörfler marking")
+    args = parser.parse_args()
+
+    THETA = args.theta
+
+    # ------------------------------------------------
+    # Setup
+    # ------------------------------------------------
+    base_path = Path('data')
+    path_to_elements = base_path / Path('elements.dat')
+    path_to_coordinates = base_path / Path('coordinates.dat')
+    path_to_dirichlet = base_path / Path('dirichlet.dat')
+
+    base_results_path = Path('results/experiment_01') / Path(f'theta_{THETA}')
+
+    coordinates, elements = io_helpers.read_mesh(
+        path_to_coordinates=path_to_coordinates,
+        path_to_elements=path_to_elements,
+        shift_indices=False)
+    boundaries = [io_helpers.read_boundary_condition(
+        path_to_boundary=path_to_dirichlet,
+        shift_indices=False)]
+
+    energies = []
+    n_elements = []
+    dts = []
+
+    # initial refinement
+    # ------------------
+    n_initial_refinements = N_INITIAL_REFINEMENTS
+    for _ in range(n_initial_refinements):
+        marked = np.arange(elements.shape[0])
+        coordinates, elements, boundaries, _ = \
+            refinement.refineNVB(coordinates=coordinates,
+                                 elements=elements,
+                                 marked_elements=marked,
+                                 boundary_conditions=boundaries)
+
+    # solve exactly on the initial mesh
+    solution, energy = solvers.solve_laplace(
+        coordinates=coordinates,
+        elements=elements,
+        dirichlet=boundaries[0],
+        neumann=np.array([]),
+        f=f, g=None, uD=uD)
+    energies.append(energy)
+    n_elements.append(elements.shape[0])
+    dts.append(0.)
+
+    dump_object(obj=solution,
+                path_to_file=base_results_path / Path('0/solution.pkl'))
+    dump_object(obj=elements,
+                path_to_file=base_results_path / Path('0/elements.pkl'))
+    dump_object(obj=coordinates,
+                path_to_file=base_results_path / Path('0/coordinates.pkl'))
+    dump_object(obj=boundaries,
+                path_to_file=base_results_path / Path('0/boundaries.pkl'))
+
+    # ------------------------------------------------
+    # variational adaptivity
+    # ------------------------------------------------
+
+    for n_refinement in np.arange(1, N_REFINEMENTS + 1):
+        start_time = time.process_time_ns()
+
+        # compute all local energy gains
+        # ------------------------------
+        element_to_neighbours = get_element_to_neighbours(elements=elements)
+        local_energy_gains = algo_4_1.get_all_local_enery_gains(
+            coordinates=coordinates,
+            elements=elements,
+            boundaries=boundaries,
+            current_iterate=solution,
+            element_to_neighbours=element_to_neighbours,
+            uD=uD,
+            rhs_function=f, lamba_a=1)
+
+        # mark elements to be refined, then refine
+        # ---------------------------------------
+        marked = markers.doerfler_marking(local_energy_gains, theta=THETA)
+        coordinates, elements, boundaries, solution = refinement.refineNVB(
+            coordinates=coordinates,
+            elements=elements,
+            marked_elements=(marked != 0),
+            boundary_conditions=boundaries,
+            to_embed=solution)
+
+        # solve linear problem exactly on current mesh
+        # --------------------------------------------
+        solution, energy = solvers.solve_laplace(
+            coordinates=coordinates,
+            elements=elements,
+            dirichlet=boundaries[0],
+            neumann=np.array([]),
+            f=f, g=None, uD=uD)
+
+        end_time = time.process_time_ns()
+        dts.append((end_time - start_time)*1e-9)
+
+        dump_object(obj=solution, path_to_file=base_results_path /
+                    Path(f'{n_refinement}/solution.pkl'))
+        dump_object(obj=elements, path_to_file=base_results_path /
+                    Path(f'{n_refinement}/elements.pkl'))
+        dump_object(obj=coordinates, path_to_file=base_results_path /
+                    Path(f'{n_refinement}/coordinates.pkl'))
+        dump_object(obj=boundaries, path_to_file=base_results_path /
+                    Path(f'{n_refinement}/boundaries.pkl'))
+
+        energies.append(energy)
+        n_elements.append(elements.shape[0])
+
+    # ------------------------------------------------
+    # wrap-up
+    # ------------------------------------------------
+
+    energies = np.array(energies)
+    n_elements = np.array(n_elements)
+    dts = np.array(dts)
+
+    result = {
+        'dt (s)': dts,
+        'n_elements': n_elements,
+        'energies': energies,
+        'theta': THETA
+    }
+
+    dump_object(obj=pd.DataFrame(result),
+                path_to_file=base_results_path / Path('results.pkl'))
+
+
+# ----------------
+# helper functions
+# ----------------
+
+
+def dump_object(obj, path_to_file: Path) -> None:
+    path_to_file.parent.mkdir(parents=True, exist_ok=True)
+    # save result as a pickle dump of pd.dataframe
+    with open(path_to_file, "wb") as file:
+        # Dump the DataFrame into the file using pickle
+        pickle.dump(obj, file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ variational_adaptivity/square_with_singularity/plot_error.py
@@ -0,0 +1,97 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
+from p1afempy import solvers
+from scipy.optimize import curve_fit
+
+energy_squared_exact = 0.035144253738788451
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, required=True,
+                        help="path to the result's `.pkl` files")
+    parser.add_argument("-o", type=str, required=False,
+                        default='energy_error_squared.pdf',
+                        help="path to the outputted plot")
+    args = parser.parse_args()
+
+    base_path = Path(args.path)
+    output_path = Path(args.o)
+
+    energies_squared = []
+    n_elements = []
+    n_coordinates = []
+    n_dofs = []
+
+    for n_dir in base_path.iterdir():
+        # exclude the initial solution
+        if not n_dir.is_dir() or n_dir.name == '0':
+            continue
+        path_to_coordinates = \
+            n_dir / Path('coordinates.pkl')
+        path_to_elements = \
+            n_dir / Path('elements.pkl')
+        path_to_solution = \
+            n_dir / Path('solution.pkl')
+        path_to_boundaries = \
+            n_dir / Path('boundaries.pkl')
+
+        with open(path_to_coordinates, mode='rb') as file:
+            coordinates = pickle.load(file=file)
+        with open(path_to_elements, mode='rb') as file:
+            elements = pickle.load(file=file)
+        with open(path_to_solution, mode='rb') as file:
+            solution = pickle.load(file=file)
+        with open(path_to_boundaries, mode='rb') as file:
+            boundaries = pickle.load(file=file)[0]
+
+        stiffness_matrix = solvers.get_stiffness_matrix(
+            coordinates=coordinates, elements=elements)
+
+        energy_squared = solution.dot(stiffness_matrix.dot(solution))
+
+        energies_squared.append(energy_squared)
+        n_elements.append(elements.shape[0])
+        n_coordinates.append(coordinates.shape[0])
+        n_dofs.append(coordinates.shape[0] - (boundaries.shape[0] + 1))
+
+    energies_squared = np.array(energies_squared)
+    n_elements = np.array(n_elements)
+    n_coordinates = np.array(n_coordinates)
+    n_dofs = np.array(n_dofs)
+
+    sort_indices = n_dofs.argsort()
+
+    energies_squared = energies_squared[sort_indices]
+    n_dofs = n_dofs[sort_indices]
+
+    errs_squared = energy_squared_exact - energies_squared
+
+    def model(x, m):
+        return -x + m
+    popt, pcov = curve_fit(model, np.log(n_dofs), np.log(errs_squared))
+    m_optimized = popt[0]
+
+    plt.rcParams["mathtext.fontset"] = "cm"
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['axes.titlesize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'$n_{\text{DOF}}$')
+    ax.set_ylabel(r'$\| u_h - u \|_a^2$')
+    ax.grid(True)
+    ax.loglog(n_dofs, errs_squared, 'b--', marker='s', markerfacecolor=(0, 0, 1, 0.5), markersize=4, linewidth=0.5)
+    ax.loglog(n_dofs, np.exp(model(np.log(n_dofs), m_optimized)), 'k--', linewidth=0.8) #, label=r'$\propto -\log n_{\text{elements}} $')
+    # ax.legend()
+
+    fig.savefig(output_path, dpi=300)
+
+
+if __name__ == '__main__':
+    main()
