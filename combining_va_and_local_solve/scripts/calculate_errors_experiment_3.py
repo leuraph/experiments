90bed0246c38c990f534cc302e244895a145694f
rleu <raphaelleu95@gmail.com>
2024-08-19 09:49:46 +0200

    chore: calc errors for exp3

--- combining_va_and_local_solve/scripts/calculate_errors_experiment_3.py
+++ combining_va_and_local_solve/scripts/calculate_errors_experiment_3.py
@@ -10,36 +10,34 @@ from triangle_cubature.cubature_rule import CubatureRuleEnum
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--theta", type=float, required=True)
-    parser.add_argument("--fudge", type=float, required=True)
-    parser.add_argument("--experiment", type=int, required=True)
     args = parser.parse_args()
 
     theta = args.theta
-    fudge = args.fudge
-    experiment = args.experiment
 
     base_result_path = Path(
-        f'results/experiment_{experiment}/theta-{theta}_fudge-{fudge}')
+        f'results/experiment_3/theta-{theta}')
 
-    for path_to_result in tqdm(list(base_result_path.iterdir())):
-        path_to_solution = path_to_result / Path('solution.pkl')
-        path_to_elements = path_to_result / Path('elements.pkl')
-        path_to_coordinates = path_to_result / Path('coordinates.pkl')
+    for path_to_ndof_dir in tqdm(list(base_result_path.iterdir())):
+        if path_to_ndof_dir.is_dir():
+            path_to_elements = path_to_ndof_dir / Path('elements.pkl')
+            path_to_coordinates = path_to_ndof_dir / Path('coordinates.pkl')
+            elements = load_dump(path_to_dump=path_to_elements)
+            coordinates = load_dump(path_to_dump=path_to_coordinates)
 
-        solution = load_dump(path_to_dump=path_to_solution)
-        elements = load_dump(path_to_dump=path_to_elements)
-        coordinates = load_dump(path_to_dump=path_to_coordinates)
+            for n_sweep_dir in list(path_to_ndof_dir.iterdir()):
+                path_to_solution = n_sweep_dir / Path('solution.pkl')
+                solution = load_dump(path_to_dump=path_to_solution)
 
-        energy_norm_error_squared = calculate_energy_norm_error(
-            current_iterate=solution,
-            gradient_u=grad_u,
-            elements=elements,
-            coordinates=coordinates,
-            cubature_rule=CubatureRuleEnum.SMPLX1)
+                energy_norm_error_squared = calculate_energy_norm_error(
+                    current_iterate=solution,
+                    gradient_u=grad_u,
+                    elements=elements,
+                    coordinates=coordinates,
+                    cubature_rule=CubatureRuleEnum.SMPLX1)
 
-        dump_object(
-            obj=energy_norm_error_squared,
-            path_to_file=path_to_result / Path('energy_norm_error.pkl'))
+                dump_object(
+                    obj=energy_norm_error_squared,
+                    path_to_file=n_sweep_dir / Path('energy_norm_error.pkl'))
 
 
 if __name__ == '__main__':
