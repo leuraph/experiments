228e0c8fb54bff5763c4e9763087d7d656397456
rleu <raphaelleu95@gmail.com>
2024-08-20 14:45:11 +0200

    chore: adapted logic of error calculation for generic script

--- combining_va_and_local_solve/calculate_errors_for_exp.py
+++ combining_va_and_local_solve/calculate_errors_for_exp.py
@@ -9,18 +9,14 @@ from triangle_cubature.cubature_rule import CubatureRuleEnum
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--theta", type=float, required=True)
-    parser.add_argument("--sweeps", type=int, required=True)
+    parser.add_argument("--path", type=str, required=True)
     args = parser.parse_args()
 
-    theta = args.theta
-    sweeps = args.sweeps
+    base_result_path = Path(args.path)
 
-    base_result_path = Path(
-        f'results/experiment_3/theta-{theta}_sweeps-{sweeps}')
-
-    for path_to_ndof_dir in tqdm(list(base_result_path.iterdir())):
+    for path_to_ndof_dir in list(base_result_path.iterdir()):
         if path_to_ndof_dir.is_dir():
+            print(f'considering subdirectory: {path_to_ndof_dir.name}')
             path_to_elements = path_to_ndof_dir / Path('elements.pkl')
             path_to_coordinates = path_to_ndof_dir / Path('coordinates.pkl')
             path_to_exact_solution = (
@@ -42,7 +38,7 @@ def main() -> None:
                     path_to_ndof_dir /
                     Path('energy_norm_error_squared_exact.pkl')))
 
-            for n_sweep_dir in list(path_to_ndof_dir.iterdir()):
+            for n_sweep_dir in tqdm(list(path_to_ndof_dir.iterdir())):
                 if n_sweep_dir.is_dir():
                     path_to_solution = n_sweep_dir / Path('solution.pkl')
                     solution = load_dump(path_to_dump=path_to_solution)
