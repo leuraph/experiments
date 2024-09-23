e7f0ccd9094753850be8d7a5f2e8b1492ea786e1
rleu <raphaelleu95@gmail.com>
2024-09-23 10:48:36 +0200

    chore: added error calc for all exps in one cript

--- combining_va_and_local_solve/calculate_errors_for_exp.py
+++ combining_va_and_local_solve/calculate_errors_for_exp.py
@@ -23,10 +23,47 @@ def main() -> None:
     print(f'processing some results for experiment number {experiment_number}')
     print('------------------------------------------------')
 
-    if experiment_number in [4, 5, 6, 7, 8, 9]:
+    if experiment_number in [1, 2]:
+
+        perform_calculation_for_experiment_without_sweeps(
+            base_result_path)
+
+    if experiment_number in [3, 4, 5, 6, 7, 8, 9]:
 
         perform_calculations_for_experiment_with_sweeps(
             base_result_path)
+
+
+def perform_calculation_for_experiment_without_sweeps(
+        base_result_path):
+    # Cubature rule used for approximating
+    # energy norm distance to exact solution
+    cubature_rule = CubatureRuleEnum.SMPLX1
+
+    for path_to_n_sweep_dir in list(base_result_path.iterdir()):
+        if path_to_n_sweep_dir.is_dir():
+            path_to_elements = path_to_n_sweep_dir / Path('elements.pkl')
+            path_to_coordinates = path_to_n_sweep_dir / Path('coordinates.pkl')
+            path_to_approximate_solution = path_to_n_sweep_dir \
+                / Path('solution.pkl')
+
+            elements = load_dump(path_to_dump=path_to_elements)
+            coordinates = load_dump(path_to_dump=path_to_coordinates)
+            approximate_solution = load_dump(
+                path_to_dump=path_to_approximate_solution)
+
+            energy_norm_error_squared_exact = calculate_energy_norm_error(
+                current_iterate=approximate_solution,
+                gradient_u=grad_u,
+                elements=elements,
+                coordinates=coordinates,
+                cubature_rule=cubature_rule)
+
+            dump_object(
+                obj=energy_norm_error_squared_exact,
+                path_to_file=(
+                    path_to_n_sweep_dir /
+                    Path('energy_norm_error_squared_exact.pkl')))
 
 
 def perform_calculations_for_experiment_with_sweeps(
