9a02f2c0ca74cdaa9ea556fd170146ec95ff070f
rleu <raphaelleu95@gmail.com>
2025-01-06 08:35:00 +0100

    chore: setting up random refinement experiment

---
+++ random_refinement/experiment_01.py
@@ -0,0 +1,144 @@
+import numpy as np
+from p1afempy import io_helpers, solvers
+from triangle_cubature.cubature_rule import CubatureRuleEnum
+from p1afempy.refinement import refineNVB
+
+from pathlib import Path
+from configuration import uD, f
+import pickle
+import argparse
+from tqdm import tqdm
+
+
+def main() -> None:
+    np.random.seed(42)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--percentage", type=float, required=True,
+                        help="what percentage of all elemets get "
+                        "randomly selected for refinement")
+    args = parser.parse_args()
+
+    PERCENTAGE = args.percentage
+
+    # ------------------------------------------------
+    # Setup
+    # ------------------------------------------------
+    base_path = Path('data')
+    path_to_elements = base_path / Path('elements.dat')
+    path_to_coordinates = base_path / Path('coordinates.dat')
+    path_to_dirichlet = base_path / Path('dirichlet.dat')
+
+    base_results_path = Path(
+        'results/experiment_01') / Path(f'percentage_{PERCENTAGE}')
+
+    coordinates, elements = io_helpers.read_mesh(
+        path_to_coordinates=path_to_coordinates,
+        path_to_elements=path_to_elements,
+        shift_indices=False)
+    boundaries = [io_helpers.read_boundary_condition(
+        path_to_boundary=path_to_dirichlet,
+        shift_indices=False)]
+
+    # initial refinement
+    # ------------------
+    n_initial_refinements = 5
+    for _ in range(n_initial_refinements):
+        marked = np.arange(elements.shape[0])
+        coordinates, elements, boundaries, _ = \
+            refineNVB(
+                coordinates=coordinates,
+                elements=elements,
+                marked_elements=marked,
+                boundary_conditions=boundaries)
+
+    # solve exactly on the initial mesh
+    solution, _ = solvers.solve_laplace(
+        coordinates=coordinates,
+        elements=elements,
+        dirichlet=boundaries[0],
+        neumann=np.array([]),
+        f=f, g=None, uD=uD)
+
+    n_vertices = coordinates.shape[0]
+    indices_of_free_nodes = np.setdiff1d(
+        ar1=np.arange(n_vertices),
+        ar2=np.unique(boundaries[0].flatten()))
+    free_nodes = np.zeros(n_vertices, dtype=bool)
+    free_nodes[indices_of_free_nodes] = 1
+    n_dofs = np.sum(free_nodes)
+
+    dump_object(obj=solution,
+                path_to_file=base_results_path / Path(
+                    f'{n_dofs}/galerkin_solution.pkl'))
+    dump_object(obj=elements,
+                path_to_file=base_results_path / Path(
+                    f'{n_dofs}/elements.pkl'))
+    dump_object(obj=coordinates,
+                path_to_file=base_results_path / Path(
+                    f'{n_dofs}/coordinates.pkl'))
+    dump_object(obj=boundaries,
+                path_to_file=base_results_path / Path(
+                    f'{n_dofs}/boundaries.pkl'))
+
+    # -----------------------------------
+    # random refinements and exact solves
+    # -----------------------------------
+
+    n_refinements = 5
+    for _ in tqdm(range(n_refinements)):
+
+        # random refinement
+        # -----------------
+        # Calculate the number of elements to mark
+        n_elements = elements.shape[0]
+        n_to_mark = int(np.ceil(PERCENTAGE * n_elements))
+
+        # Randomly select elements to mark
+        marked = np.random.choice(n_elements, size=n_to_mark, replace=False)
+
+        coordinates, elements, boundaries, _ = \
+            refineNVB(
+                coordinates=coordinates,
+                elements=elements,
+                marked_elements=marked,
+                boundary_conditions=boundaries)
+
+        # solve linear problem exactly on current mesh
+        # --------------------------------------------
+        solution, _ = solvers.solve_laplace(
+            coordinates=coordinates,
+            elements=elements,
+            dirichlet=boundaries[0],
+            neumann=np.array([]),
+            f=f, g=None, uD=uD,
+            cubature_rule=CubatureRuleEnum.DAYTAYLOR)
+
+        n_vertices = coordinates.shape[0]
+        indices_of_free_nodes = np.setdiff1d(
+            ar1=np.arange(n_vertices),
+            ar2=np.unique(boundaries[0].flatten()))
+        free_nodes = np.zeros(n_vertices, dtype=bool)
+        free_nodes[indices_of_free_nodes] = 1
+        n_dofs = np.sum(free_nodes)
+
+        dump_object(obj=solution, path_to_file=base_results_path /
+                    Path(f'{n_dofs}/galerkin_solution.pkl'))
+        dump_object(obj=elements, path_to_file=base_results_path /
+                    Path(f'{n_dofs}/elements.pkl'))
+        dump_object(obj=coordinates, path_to_file=base_results_path /
+                    Path(f'{n_dofs}/coordinates.pkl'))
+        dump_object(obj=boundaries, path_to_file=base_results_path /
+                    Path(f'{n_dofs}/boundaries.pkl'))
+
+
+def dump_object(obj, path_to_file: Path) -> None:
+    path_to_file.parent.mkdir(parents=True, exist_ok=True)
+    # save result as a pickle dump of pd.dataframe
+    with open(path_to_file, "wb") as file:
+        # Dump the DataFrame into the file using pickle
+        pickle.dump(obj, file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ random_refinement/plot_energy_errors_galerkin_decay.py
@@ -0,0 +1,83 @@
+from load_save_dumps import load_dump
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
+from scipy.optimize import curve_fit
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, required=True,
+                        help="path to the folder including "
+                        "all experiment folders")
+    parser.add_argument("-o", type=str, required=False,
+                        default='energy_error_squared.pdf',
+                        help="path to the outputted plot")
+    args = parser.parse_args()
+
+    base_path = Path(args.path)
+    output_path = Path(args.o)
+
+    energy_norm_errors_squared_galerkin_with_orthogonality = []
+    n_dofs = []
+
+    for n_dofs_dir in base_path.iterdir():
+        # exclude the initial solution
+        if not n_dofs_dir.is_dir():
+            continue
+
+        n_dof = int(n_dofs_dir.name)
+        path_to_energy_norm_error_squared_galerkin_with_orthogonality = \
+            n_dofs_dir / Path(
+                'energy_norm_error_squared_galerkin_with_orthogonality.pkl')
+
+        energy_norm_error_squared_galerkin_with_orthogonality = \
+            load_dump(path_to_dump=path_to_energy_norm_error_squared_galerkin_with_orthogonality)
+
+        n_dofs.append(n_dof)
+        energy_norm_errors_squared_galerkin_with_orthogonality.append(
+            energy_norm_error_squared_galerkin_with_orthogonality)
+
+    energy_norm_errors_squared_galerkin_with_orthogonality = np.array(
+        energy_norm_errors_squared_galerkin_with_orthogonality)
+    n_dofs = np.array(n_dofs)
+
+    sort_n_dof = n_dofs.argsort()
+
+    energy_norm_errors_squared_galerkin_with_orthogonality = \
+        energy_norm_errors_squared_galerkin_with_orthogonality[sort_n_dof]
+    n_dofs = n_dofs[sort_n_dof]
+
+    def model(x, m):
+        return -x + m
+    popt, pcov = curve_fit(
+        model,
+        np.log(n_dofs),
+        np.log(energy_norm_errors_squared_galerkin_with_orthogonality))
+    m_optimized = popt[0]
+
+    plt.rcParams["mathtext.fontset"] = "cm"
+    plt.rcParams['xtick.labelsize'] = 16
+    plt.rcParams['ytick.labelsize'] = 16
+    plt.rcParams['axes.labelsize'] = 20
+    plt.rcParams['axes.titlesize'] = 12
+    plt.rcParams['legend.fontsize'] = 16
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel(r'$n_{\text{DOF}}$')
+    ax.set_ylabel(r'$\| u_h - u \|_a^2$')
+    ax.grid(True)
+    ax.loglog(
+        n_dofs, energy_norm_errors_squared_galerkin_with_orthogonality,
+        'b--', marker='s', label='with orthogonality',
+        markerfacecolor=(0, 0, 1, 0.5), markersize=4, linewidth=0.5)
+    ax.loglog(n_dofs, np.exp(model(np.log(n_dofs), m_optimized)),
+              'k--', linewidth=0.8)
+    ax.legend()
+
+    fig.savefig(output_path, dpi=300, bbox_inches="tight")
+
+
+if __name__ == '__main__':
+    main()
