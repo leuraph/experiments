1741340b558f4888e9c1f80092bd37ee6f87e382
rleu <raphaelleu95@gmail.com>
2025-01-20 17:41:01 +0100

    refactor: remove obsolete code

--- edge_based_variational_adaptivity/gauss_kick/experiment_02.py
+++ edge_based_variational_adaptivity/gauss_kick/experiment_02.py
@@ -8,8 +8,6 @@ from configuration import uD, f
 from ismember import is_row_in
 import pickle
 import argparse
-import matplotlib.pyplot as plt
-from matplotlib import cm
 from variational_adaptivity.edge_based_variational_adaptivity import \
     get_energy_gains
 from triangle_cubature.cubature_rule import CubatureRuleEnum
@@ -183,37 +181,6 @@ def dump_object(obj, path_to_file: Path) -> None:
     with open(path_to_file, "wb") as file:
         # Dump the DataFrame into the file using pickle
         pickle.dump(obj, file)
-
-
-def show_solution(coordinates, solution):
-    plt.rcParams["mathtext.fontset"] = "cm"
-    plt.rcParams['xtick.labelsize'] = 12
-    plt.rcParams['ytick.labelsize'] = 12
-    plt.rcParams['axes.labelsize'] = 20
-    plt.rcParams['axes.titlesize'] = 12
-    plt.rcParams['legend.fontsize'] = 12
-
-    x_coords, y_coords = zip(*coordinates)
-
-    # Create a 3D scatter plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Plot the points with scalar values as colors (adjust colormap as needed)
-    _ = ax.plot_trisurf(x_coords, y_coords, solution, linewidth=0.2,
-                        antialiased=True, cmap=cm.viridis)
-    # Add labels to the axes
-    ax.set_xlabel(r'$x$')
-    ax.set_ylabel(r'$y$')
-    ax.set_zlabel(r'$z$')
-
-    ax.set_xticks([0, 1])
-    ax.set_yticks([0, 1])
-    # ax.set_zticks([0, 0.02, 0.04, 0.06])
-
-    # Show and save the plot
-    # fig.savefig(out_path, dpi=300)
-    plt.show()
 
 
 if __name__ == '__main__':
