69ab07cb651316005c4038dbb3db31954516570a
rleu <raphaelleu95@gmail.com>
2025-01-22 08:28:34 +0100

    chore: add `energy_history`

--- edge_based_variational_adaptivity/gauss_kick/custom_callback.py
+++ edge_based_variational_adaptivity/gauss_kick/custom_callback.py
@@ -15,15 +15,18 @@ class ConvergedException(Exception):
     energy_gains: np.ndarray
     last_iterate: np.ndarray
     n_iterations_done: int
+    energy_history: list[float]
 
     def __init__(
             self,
             energy_gains: np.ndarray,
             last_iterate: np.ndarray,
-            n_iterations_done: int):
+            n_iterations_done: int = None,
+            energy_history: list[float] = None):
         self.energy_gains = energy_gains
         self.last_iterate = last_iterate
         self.n_iterations_done = n_iterations_done
+        self.energy_history = energy_history
 
 
 class CustomCallBack():
@@ -274,6 +277,7 @@ class EnergyTailOffCustomCallback(CustomCallBack):
     fudge: float
     accumulated_energy_gain: float
     verbose: bool
+    energy_history: list[float]
 
     def __init__(
             self,
@@ -295,6 +299,7 @@ class EnergyTailOffCustomCallback(CustomCallBack):
         self.fudge = fudge
         self.accumulated_energy_gain = 0.
         self.energy_of_last_iterate = energy_of_initial_guess
+        self.energy_history = [energy_of_initial_guess]
 
     def calculate_energy(self, current_iterate) -> float:
         return (
@@ -307,6 +312,7 @@ class EnergyTailOffCustomCallback(CustomCallBack):
 
         current_energy = self.calculate_energy(
             current_iterate=current_iterate)
+        self.energy_history.append(current_energy)
         energy_gain_iteration = self.energy_of_last_iterate - current_energy
         self.accumulated_energy_gain += energy_gain_iteration
 
@@ -323,7 +329,8 @@ class EnergyTailOffCustomCallback(CustomCallBack):
             converged_exception = ConvergedException(
                 energy_gains=energy_gains,
                 last_iterate=current_iterate,
-                n_iterations_done=self.n_iterations_done)
+                n_iterations_done=self.n_iterations_done,
+                energy_history=self.energy_history)
             raise converged_exception
 
         # keep energy considerations in memory
