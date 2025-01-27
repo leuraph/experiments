1bb35625da028b6ae7ba330312e5e5a0e7d62cf1
rleu <raphaelleu95@gmail.com>
2025-01-27 16:22:14 +0100

    chore: adapted script to plot both errs for L-shape

--- edge_based_variational_adaptivity/l_shape_with_singularity/plot_energy_errors_exp_05.py
+++ edge_based_variational_adaptivity/l_shape_with_singularity/plot_energy_errors_exp_05.py
@@ -16,8 +16,16 @@ def main() -> None:
                         help="path to the outputted plot")
     args = parser.parse_args()
 
-    path_to_exp = Path(args.path)
+    results_path = Path(args.path)
     output_path = Path(args.o)
+
+    energy_norm_errors_squared_galerkin_to_exact, \
+        energy_norm_errors_squared_last_iterate_to_galerkin, \
+        n_dofs = get_energy_norm_errors_squared_and_dofs(
+            results_path=results_path)
+
+    # settinng plot params
+    # --------------------
 
     plt.rcParams["mathtext.fontset"] = "cm"
     plt.rcParams['xtick.labelsize'] = 16
@@ -28,54 +36,56 @@ def main() -> None:
 
     fig, ax = plt.subplots()
     ax.set_xlabel(r'$n_{\text{DOF}}$')
-    ax.set_ylabel(r'$\| u_h - u \|_a^2$')
+    # ax.set_ylabel(r'$\| u_h - u_N^{n^\star} \|_a^2$')
     ax.grid(True)
 
-    errs_arrays = []
-    n_dofs_arrays = []
-    thetas = []
+    # plotting |u_h - u_N^{n^\star}|_a^2
+    # ----------------------------------
+    def model(x, m):
+        return -x + m
+    popt, pcov = curve_fit(
+        model,
+        np.log(n_dofs[1:]),
+        np.log(energy_norm_errors_squared_last_iterate_to_galerkin[1:]))
+    m_optimized = popt[0]
 
-    for theta_path in path_to_exp.iterdir():
-        if not theta_path.is_dir():
-            continue
-        energy_norm_errors_squared_galerkin_with_orthogonality, n_dofs = \
-            get_energy_norm_errors_squared_and_dofs(theta_path=theta_path)
-        theta = get_theta_value_from_path(path=theta_path)
-
-        if theta not in [0.1, 0.3, 0.5, 0.7, 0.9]:
-            continue
-
-        errs_arrays.append(
-            energy_norm_errors_squared_galerkin_with_orthogonality)
-        n_dofs_arrays.append(n_dofs)
-        thetas.append(theta)
-
-    # Loop through the pairs and plot each
-    for i, (n_dofs, errs, theta) in enumerate(
-            zip(n_dofs_arrays, errs_arrays, thetas)):
-        # Generate a unique color for each pair
-        color = plt.cm.tab10(i % 10)  # Cycle through a colormap (tab10)
-
-        ax.loglog(
-            n_dofs, errs,
-            label=rf'$\theta = {theta}$',
+    color = plt.cm.tab10(1)
+    ax.loglog(
+            n_dofs[1:],
+            energy_norm_errors_squared_last_iterate_to_galerkin[1:],
             linestyle='--',  # Dotted line
             marker='s',     # Square markers
             color=color,    # Line and marker color
             markerfacecolor=color,  # Marker fill color
             markeredgecolor=color,  # Marker outline color
             alpha=0.5,       # Transparency for markers
-            markersize=4, linewidth=0.5
-        )
+            label=r'$\|u_h - u_N^{n^\star}\|^2_a$',
+            markersize=5, linewidth=1.0)
+    ax.loglog(n_dofs[1:], np.exp(model(np.log(n_dofs[1:]), m_optimized)),
+              'k--', linewidth=0.8)
+    # -------------------------------------
 
+    # plotting |u - u_h|_a^2
+    # ----------------------
     def model(x, m):
         return -x + m
     popt, pcov = curve_fit(
         model,
         np.log(n_dofs),
-        np.log(errs))
+        np.log(energy_norm_errors_squared_galerkin_to_exact))
     m_optimized = popt[0]
 
+    color = plt.cm.tab10(2)
+    ax.loglog(
+            n_dofs, energy_norm_errors_squared_galerkin_to_exact,
+            linestyle='--',  # Dotted line
+            marker='s',     # Square markers
+            color=color,    # Line and marker color
+            markerfacecolor=color,  # Marker fill color
+            markeredgecolor=color,  # Marker outline color
+            alpha=0.5,       # Transparency for markers
+            label=r'$\|u - u_h\|^2_a$',
+            markersize=5, linewidth=1.0)
     ax.loglog(n_dofs, np.exp(model(np.log(n_dofs), m_optimized)),
               'k--', linewidth=0.8)
 
@@ -85,12 +95,33 @@ def main() -> None:
 
 
 def get_energy_norm_errors_squared_and_dofs(
-        theta_path: Path) -> tuple[np.ndarray, np.ndarray]:
+        results_path: Path) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    reads and returns the energy norm errors squared of
+    the Galerkin solution and the last iterate.
 
-    energy_norm_errors_squared_galerkin_with_orthogonality = []
+    note
+    ----
+    as the exact solution u(x, y) is unavailbale in this experiment,
+    we need to calculate both |u - u_h| and |u_n - u_h| separately.
+
+    returns
+    -------
+    energy_norm_errors_squared_galerkin_to_exact: np.ndarray
+        energy norm errors squared of Galerkin solutions to the exact solution
+        using Galerkin Orthogonality
+    energy_norm_errors_squared_last_iterate_to_galerkin: np.ndarray
+        energy norm error squared of last iterate to the Galerkin solution
+        of the corresponding meshes
+    n_dofs: np.ndarray
+        number of degrees of freedom
+    """
+
+    energy_norm_errors_squared_galerkin_to_exact = []
+    energy_norm_errors_squared_last_iterate_to_galerkin = []
     n_dofs = []
 
-    for n_dofs_dir in theta_path.iterdir():
+    for n_dofs_dir in results_path.iterdir():
         if not n_dofs_dir.is_dir():
             continue
 
@@ -98,25 +129,38 @@ def get_energy_norm_errors_squared_and_dofs(
         path_to_energy_norm_error_squared_galerkin_with_orthogonality = \
             n_dofs_dir / Path(
                 'energy_norm_error_squared_galerkin_with_orthogonality.pkl')
+        path_to_energy_norm_error_squared_last_iterate_to_galerkin = \
+            n_dofs_dir / Path(
+                'energy_norm_error_squared_last_iterate_to_galerkin.pkl')
 
         energy_norm_error_squared_galerkin_with_orthogonality = \
             load_dump(path_to_dump=path_to_energy_norm_error_squared_galerkin_with_orthogonality)
+        energy_norm_error_squared_last_iterate_to_galerkin = \
+            load_dump(path_to_dump=path_to_energy_norm_error_squared_last_iterate_to_galerkin)
 
         n_dofs.append(n_dof)
-        energy_norm_errors_squared_galerkin_with_orthogonality.append(
+        energy_norm_errors_squared_galerkin_to_exact.append(
             energy_norm_error_squared_galerkin_with_orthogonality)
+        energy_norm_errors_squared_last_iterate_to_galerkin.append(
+            energy_norm_error_squared_last_iterate_to_galerkin)
 
-    energy_norm_errors_squared_galerkin_with_orthogonality = np.array(
-        energy_norm_errors_squared_galerkin_with_orthogonality)
+    energy_norm_errors_squared_galerkin_to_exact = np.array(
+        energy_norm_errors_squared_galerkin_to_exact)
+    energy_norm_errors_squared_last_iterate_to_galerkin = np.array(
+        energy_norm_errors_squared_last_iterate_to_galerkin)
     n_dofs = np.array(n_dofs)
 
     sort_n_dof = n_dofs.argsort()
 
-    energy_norm_errors_squared_galerkin_with_orthogonality = \
-        energy_norm_errors_squared_galerkin_with_orthogonality[sort_n_dof]
+    energy_norm_errors_squared_galerkin_to_exact = \
+        energy_norm_errors_squared_galerkin_to_exact[sort_n_dof]
+    energy_norm_errors_squared_last_iterate_to_galerkin = \
+        energy_norm_errors_squared_last_iterate_to_galerkin[sort_n_dof]
     n_dofs = n_dofs[sort_n_dof]
 
-    return energy_norm_errors_squared_galerkin_with_orthogonality, n_dofs
+    return energy_norm_errors_squared_galerkin_to_exact, \
+        energy_norm_errors_squared_last_iterate_to_galerkin, \
+        n_dofs
 
 
 def get_theta_value_from_path(path: Path) -> float:
