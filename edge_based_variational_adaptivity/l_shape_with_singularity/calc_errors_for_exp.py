02ceff6992543dfbe10b093e60fa2647ea757faf
rleu <raphaelleu95@gmail.com>
2025-01-27 15:41:35 +0100

    chore: adapted script to L-shape experiment

--- edge_based_variational_adaptivity/l_shape_with_singularity/calc_errors_for_exp.py
+++ edge_based_variational_adaptivity/l_shape_with_singularity/calc_errors_for_exp.py
@@ -22,31 +22,21 @@ def main() -> None:
     with open(args.energy_path) as f:
         energy_norm_squared_exact = float(f.readline())
 
-    # Cubature rule used for approximating
-    # energy norm distance to exact solution
-    cubature_rule = CubatureRuleEnum.DAYTAYLOR
-
     # extracting the experiment number as integer
     pattern = r"experiment_(\d+)"
     match = re.search(pattern, str(base_result_path))
     experiment_number = int(match.group(1))
 
-    if experiment_number in [3, 4, 5]:
+    if experiment_number in [5]:
         print(f'post-processing results from experiment {experiment_number}')
-        calculate_energy_norm_error_squared_last_iterate(
-            base_result_path=base_result_path,
-            cubature_rule=cubature_rule,
-            verbose=True)
+        calculate_energy_norm_error_squared_last_iterate_to_galerkin(
+            base_result_path=base_result_path, verbose=True)
         calculate_energy_norm_error_squared_galerkin_with_orthogonality(
             base_result_path=base_result_path,
             energy_norm_squared_exact=energy_norm_squared_exact,
             verbose=True)
-        calculate_energy_norm_error_squared_galerkin_without_orthogonality(
-            base_result_path=base_result_path,
-            cubature_rule=cubature_rule,
-            verbose=True)
 
-    elif experiment_number in [1, 2]:
+    elif experiment_number in [1]:
         print(f'post-processing results from experiment {experiment_number}')
         calculate_energy_norm_error_squared_galerkin_with_orthogonality(
             base_result_path=base_result_path,
@@ -59,61 +49,12 @@ def main() -> None:
             'doing nothinng...')
 
 
-def calculate_energy_norm_error_squared_last_iterate(
+def calculate_energy_norm_error_squared_last_iterate_to_galerkin(
         base_result_path: Path,
-        cubature_rule: CubatureRuleEnum,
-        verbose: bool = True
-        ) -> None:
-    if verbose:
-        print('Calculating |u - u_n|_a^2 for last iterates...')
-    for path_to_n_dofs in tqdm(list(base_result_path.iterdir())):
-        if not path_to_n_dofs.is_dir():
-            continue
-
-        # specifying paths
-        # ----------------
-        path_to_elements = path_to_n_dofs / Path('elements.pkl')
-        path_to_coordinates = path_to_n_dofs / Path('coordinates.pkl')
-        path_to_last_iterate = path_to_n_dofs \
-            / Path('last_iterate.pkl')
-
-        # loading data
-        # ------------
-        elements = load_dump(path_to_dump=path_to_elements)
-        coordinates = load_dump(path_to_dump=path_to_coordinates)
-        last_iterate = load_dump(
-            path_to_dump=path_to_last_iterate)
-
-        # calculating the energy norm errors
-        # ----------------------------------
-
-        # |u_n - u_h|^2
-        energy_norm_error_squared_last_iterate = \
-            calculate_energy_norm_error(
-                current_iterate=last_iterate,
-                gradient_u=grad_u,
-                elements=elements,
-                coordinates=coordinates,
-                cubature_rule=cubature_rule)
-
-        # saving the energy norm errors to disk
-        # -------------------------------------
-        dump_object(
-            obj=energy_norm_error_squared_last_iterate,
-            path_to_file=(
-                path_to_n_dofs /
-                Path('energy_norm_error_squared_last_iterate.pkl')))
-
-
-def calculate_energy_norm_error_squared_galerkin_without_orthogonality(
-        base_result_path: Path,
-        cubature_rule: CubatureRuleEnum,
         verbose: bool = True) -> None:
     if verbose:
         print(
-            'Calculating |u - u_h|_a^2 '
-            'without using Galerkin Orthogonality...')
-
+            'Calculating |u_n^\star - u_h|_a^2')
     for path_to_n_dofs in tqdm(list(base_result_path.iterdir())):
         if not path_to_n_dofs.is_dir():
             continue
@@ -124,6 +65,8 @@ def calculate_energy_norm_error_squared_galerkin_without_orthogonality(
         path_to_coordinates = path_to_n_dofs / Path('coordinates.pkl')
         path_to_galerkin_solution = path_to_n_dofs \
             / Path('galerkin_solution.pkl')
+        path_to_last_iterate = path_to_n_dofs \
+            / Path('last_iterate.pkl')
 
         # loading data
         # ------------
@@ -131,27 +74,27 @@ def calculate_energy_norm_error_squared_galerkin_without_orthogonality(
         coordinates = load_dump(path_to_dump=path_to_coordinates)
         galerkin_solution = load_dump(
             path_to_dump=path_to_galerkin_solution)
+        last_iterate = load_dump(
+            path_to_dump=path_to_last_iterate)
 
         # calculating the energy norm errors
         # ----------------------------------
 
-        # |u - u_h|^2 without orthogonality
-        energy_norm_error_squared_exact = calculate_energy_norm_error(
-            current_iterate=galerkin_solution,
-            gradient_u=grad_u,
-            elements=elements,
-            coordinates=coordinates,
-            cubature_rule=cubature_rule)
+        stiffness_matrix = csr_matrix(get_stiffness_matrix(
+            coordinates=coordinates, elements=elements))
 
-        # saving the energy norm errors to disk
-        # -------------------------------------
+        du = galerkin_solution - last_iterate
+        energy_norm_error_squared_last_iterate_to_galerkin =\
+            du.dot(stiffness_matrix.dot(du))
+
+        # saving the energy norm error to disk
+        # ------------------------------------
         dump_object(
-            obj=energy_norm_error_squared_exact,
+            obj=energy_norm_error_squared_last_iterate_to_galerkin,
             path_to_file=(
                 path_to_n_dofs /
                 Path(
-                    'energy_norm_error_squared_galerkin_'
-                    'without_orthogonality.pkl')))
+                    'energy_norm_error_squared_last_iterate_to_galerkin.pkl')))
 
 
 def calculate_energy_norm_error_squared_galerkin_with_orthogonality(
