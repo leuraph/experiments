b6cb7ea7596bf85b62729dee666d53bb26a2bfe5
rleu <raphaelleu95@gmail.com>
2025-02-05 09:04:55 +0100

    chore: structure exp01 with comments

--- combining_va_and_local_solve/l_shape_with_singularity/experiment_01.py
+++ combining_va_and_local_solve/l_shape_with_singularity/experiment_01.py
@@ -1,5 +1,48 @@
 def main() -> None:
-    pass
+    max_n_dofs: int = int(1e7)
+    n_initial_refinement_steps: int = 5
+
+    # read the initial data
+    # ---------------------
+    
+
+    # perform initial refinement to get a decent mesh
+    # -----------------------------------------------
+    for _ in range(n_initial_refinement_steps):
+        pass
+
+    # solve problem on initial mesh
+    # -----------------------------
+
+    # initialize the iteration with Galerkin solution on initial mesh
+    # ---------------------------------------------------------------
+
+    # perform initial EVA by hand
+    # ---------------------------
+
+    # loop until maximum number of degrees of freedom is reached
+    # ----------------------------------------------------------
+    while True:
+        # recalculate mesh specific objects / parameters
+        # ----------------------------------------------
+
+        # calculate the Galerkin solution on the current mesh
+        # ---------------------------------------------------
+
+        # perform iterations until stopping criterion is met
+        # --------------------------------------------------
+
+        # drop all the data accumulated in the corresponding results directory
+        # --------------------------------------------------------------------
+
+        # perform EVA with the last iterate
+        # ---------------------------------
+
+        # calculate the number of degrees of freedom on the new mesh
+        # ----------------------------------------------------------
+        n_dofs: int = int(1e8)
+        if n_dofs > max_n_dofs:
+            break
 
 
 if __name__ == '__main__':
