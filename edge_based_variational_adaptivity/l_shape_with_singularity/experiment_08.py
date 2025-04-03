8def37bb03bfe11ce76814d4d6b918f7310d2cb3
rleu <raphaelleu95@gmail.com>
2025-04-03 10:36:21 +0200

    bugfix: these were not obsolete after all

--- edge_based_variational_adaptivity/l_shape_with_singularity/experiment_08.py
+++ edge_based_variational_adaptivity/l_shape_with_singularity/experiment_08.py
@@ -138,6 +138,7 @@ def main() -> None:
         is_row_in(edge_to_nodes_flipped, boundaries[0])
     )
     non_boundary = np.logical_not(boundary)
+    edges = edge_to_nodes
     non_boundary_edges = edge_to_nodes[non_boundary]
 
     # free nodes / edges
@@ -277,6 +278,7 @@ def main() -> None:
             is_row_in(edge_to_nodes_flipped, boundaries[0])
         )
         non_boundary = np.logical_not(boundary)
+        edges = edge_to_nodes
         non_boundary_edges = edge_to_nodes[non_boundary]
 
         # free nodes / edges
