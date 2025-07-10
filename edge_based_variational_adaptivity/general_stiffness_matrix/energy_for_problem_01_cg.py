bb263530b8472cc9ffb0f2eb5e1c2772eb7b5d21
raphael leu <raphaelleu95@gmail.com>
2025-07-10 08:49:23 +0200

    refactor: this script can now be used for all problems

--- edge_based_variational_adaptivity/general_stiffness_matrix/energy_for_problem_01_cg.py
+++ edge_based_variational_adaptivity/general_stiffness_matrix/energy_for_problem_01_cg.py
@@ -1,11 +1,8 @@
 """
-This scripts approximates the energy for the solution of
-nabla (A(x) nabla u(x)) + u(x) = 1,
-a_11(x) = - 1,
-a_12(x) = 0,
-a_21(x) = 0,
-a_22(x) = - 1e-2,
-on (0,1)^2 with homogeneous boundary conditions.
+This scripts approximates the energy norm squared
+for the specified problem using CG iterations
+on uniformly refined meshes, where the domain
+is hard-coded to be the unit square
 """
 import numpy as np
 from p1afempy import refinement
@@ -16,16 +13,26 @@ from scipy.sparse.linalg import spsolve
 from p1afempy.refinement import refineNVB
 from scipy.sparse import csr_matrix, diags
 from scipy.sparse.linalg import cg
-from problems import get_problem_1
+from problems import get_problem
 
 
 def main() -> None:
 
     n_max_dofs = 100e6
     n_initial_refinements = 5
-    rtol = 1e-5
+    RTOL_CG = 1e-5
+    PROBLEM_N = 1
 
-    print(f'rtol in cg = {rtol}')
+    problem = get_problem(PROBLEM_N)
+
+    # Printing meta information first
+    print(f'Calculating CG approximations and their energy norm squared...')
+    print(f'')
+    print(f'parameters')
+    print(f'----------')
+    print(f'problem number = {PROBLEM_N}')
+    print(f'relative tolerance for CG = {RTOL_CG}')
+    print(f'')
 
     # mesh
     # ----
@@ -72,20 +79,21 @@ def main() -> None:
     rhs_vector = get_right_hand_side(
         coordinates=coordinates,
         elements=elements,
-        f=get_problem_1().f,
+        f=problem.f,
         cubature_rule=CubatureRuleEnum.DAYTAYLOR)
     stiffness_matrix = get_general_stiffness_matrix(
         coordinates=coordinates,
         elements=elements,
-        a_11=get_problem_1().a_11,
-        a_12=get_problem_1().a_12,
-        a_21=get_problem_1().a_21,
-        a_22=get_problem_1().a_22,
+        a_11=problem.a_11,
+        a_12=problem.a_12,
+        a_21=problem.a_21,
+        a_22=problem.a_22,
         cubature_rule=CubatureRuleEnum.DAYTAYLOR)
     mass_matrix = get_mass_matrix(
         coordinates=coordinates,
         elements=elements)
-    lhs_matrix = csr_matrix(mass_matrix + stiffness_matrix)
+    lhs_matrix = csr_matrix(
+        mass_matrix + problem.c * stiffness_matrix)
 
     galerkin_solution = np.zeros(n_vertices)
     galerkin_solution[free_nodes] = spsolve(
@@ -117,20 +125,21 @@ def main() -> None:
         general_stiffness_matrix = get_general_stiffness_matrix(
             coordinates=coordinates,
             elements=elements,
-            a_11=get_problem_1().a_11,
-            a_12=get_problem_1().a_12,
-            a_21=get_problem_1().a_21,
-            a_22=get_problem_1().a_22,
+            a_11=problem.a_11,
+            a_12=problem.a_12,
+            a_21=problem.a_21,
+            a_22=problem.a_22,
             cubature_rule=CubatureRuleEnum.DAYTAYLOR)
         mass_matrix = get_mass_matrix(
             coordinates=coordinates,
             elements=elements)
 
-        lhs_matrix = csr_matrix(general_stiffness_matrix + mass_matrix)
+        lhs_matrix = csr_matrix(
+            general_stiffness_matrix + problem.c * mass_matrix)
         rhs_vector = get_right_hand_side(
             coordinates=coordinates,
             elements=elements,
-            f=get_problem_1().f,
+            f=problem.f,
             cubature_rule=CubatureRuleEnum.DAYTAYLOR)
 
         # compute the Galerkin solution on current mesh
@@ -146,7 +155,7 @@ def main() -> None:
             b=rhs_vector[free_nodes],
             x0=current_iterate[free_nodes],
             M=M,
-            rtol=1e-5)
+            rtol=RTOL_CG)
 
         current_iterate[free_nodes] = current_iterate_on_free_nodes
 
@@ -154,7 +163,7 @@ def main() -> None:
             lhs_matrix.dot(current_iterate))
         print(
             f'nDOF = {n_dofs}, '
-            f'Galerkin solution energy norm squared = {energy_norm_squared}')
+            f'converged CG approximation energy norm squared = {energy_norm_squared}')
 
         # stop right before refining if maximum number of DOFs is reached
         if n_dofs >= n_max_dofs:
