2902cc4b96507f0b9c73207a75d93852d76bb9c3
raphael leu <raphaelleu95@gmail.com>
2025-09-08 14:53:59 +0200

    chore: added submit script

---
+++ edge_based_variational_adaptivity/plain_convergence/submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name="exp-02_theta-0.1"
+
+# number of tasks
+#SBATCH -n 1
+
+# number of cpus per task
+#SBATCH --cpus-per-task=8
+
+# (2 * 24(h) * 60(min) = 2880)
+#SBATCH --time=2880
+
+# Email when job is done or failed
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=raphael.leu@unibe.ch
+
+module purge
+module load Workspace_Home
+
+source .venv/bin/activate
+python experiment_2.py --theta 0.1
