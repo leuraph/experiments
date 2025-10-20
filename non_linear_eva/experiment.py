8de71756655b20a9609fddce629f42eb2d447584
raphael leu <raphaelleu95@gmail.com>
2025-10-20 09:18:26 +0200

    chore: looping infinitely

--- non_linear_eva/experiment.py
+++ non_linear_eva/experiment.py
@@ -19,7 +19,7 @@ from variational_adaptivity.markers import doerfler_marking
 from p1afempy.refinement import refineNVB_edge_based
 from problems import get_problem
 from custom_callback import CustomCallBack, EnergyTailOffAveragedCustomCallback, \
-    energ
+    AriolisAdaptiveDelayCustomCallback
 
 
 def validate_args(args: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
@@ -75,7 +75,7 @@ def get_custom_callback(
         )
         return callback
     elif stopping_criterion == "default":
-        def callback() -> None:
+        def callback(current_iterate: np.ndarray) -> None:
             pass
         return callback
     else:
@@ -157,7 +157,7 @@ def main() -> None:
     # ---------------------------------------
 
     # initial refinement
-    n_refinements = 5
+    n_refinements = 2
     for _ in range(n_refinements):
         marked_elements = np.arange(elements.shape[0])
         coordinates, elements, boundaries, _ = refinement.refineNVB(
@@ -176,153 +176,155 @@ def main() -> None:
 
     current_iterate = np.zeros(n_coordinates, dtype=float)
 
-    # midpoint suffices as we consider laplace operator
-    stiffness_matrix = csr_matrix(get_general_stiffness_matrix(
-        coordinates=coordinates,
-        elements=elements,
-        a_11=a_11, a_12=a_12, a_21=a_21, a_22=a_22,
-        cubature_rule=CubatureRuleEnum.DAYTAYLOR))
+    while True:
 
-    right_hand_side_vector = get_right_hand_side(
-        coordinates=coordinates,
-        elements=elements,
-        f=f,
-        cubature_rule=CubatureRuleEnum.DAYTAYLOR)
-
-    def DJ(current_iterate: np.ndarray) -> np.ndarray:
-
-        load_vector_phi = get_load_vector_of_composition_nonlinear_with_fem(
-            f=phi,
-            u=current_iterate,
+        # midpoint suffices as we consider laplace operator
+        stiffness_matrix = csr_matrix(get_general_stiffness_matrix(
             coordinates=coordinates,
             elements=elements,
+            a_11=a_11, a_12=a_12, a_21=a_21, a_22=a_22,
+            cubature_rule=CubatureRuleEnum.DAYTAYLOR))
+
+        right_hand_side_vector = get_right_hand_side(
+            coordinates=coordinates,
+            elements=elements,
+            f=f,
             cubature_rule=CubatureRuleEnum.DAYTAYLOR)
 
-        grad_J = np.zeros(n_coordinates, dtype=float)
-        grad_J_on_free_nodes = (
-            stiffness_matrix[free_nodes, :][:, free_nodes].dot(current_iterate[free_nodes])
-            +
-            load_vector_phi[free_nodes]
-            -
-            right_hand_side_vector[free_nodes]
-        )
-        grad_J[free_nodes] = grad_J_on_free_nodes
-        return grad_J
+        def DJ(current_iterate: np.ndarray) -> np.ndarray:
 
-    def J(current_iterate: np.ndarray) -> float:
-        J = (
-            0.5 * current_iterate.dot(stiffness_matrix.dot(current_iterate))
-            +
-            integrate_composition_nonlinear_with_fem(
-                f=Phi,
+            load_vector_phi = get_load_vector_of_composition_nonlinear_with_fem(
+                f=phi,
                 u=current_iterate,
                 coordinates=coordinates,
                 elements=elements,
                 cubature_rule=CubatureRuleEnum.DAYTAYLOR)
-            -
-            right_hand_side_vector.dot(current_iterate)
-        )
-        return J
 
-    # Custom Stopping Criterion: Energy Tail-Off or Others
-    # ----------------------------------------------------
-    custom_callback = get_custom_callback(
-        stopping_criterion=args.stopping_criterion, args=args)
-
-    # distinguish two cases; default and non-default stopping criteria
-    # as default criteria does not implement a custom callback and therefore
-    # does not raise on convergence
-    if args.stopping_criterion == "default":
-        gtol = GTOL  # could be None, fmin_cg will use its default
-        current_iterate, f_opt, func_calls, grad_calls, _ = \
-            fmin_cg(
-                f=J,
-                x0=current_iterate,
-                fprime=DJ,
-                full_output=True,
-                callback=custom_callback,
-                gtol=gtol)
-        n_iterations = func_calls
-    else:
-        gtol = 1e-12  # or smaller to ensure custom stopping criterion is used
-        try:
-            _, _, _, _, _ = \
-            fmin_cg(
-                f=J,
-                x0=current_iterate,
-                fprime=DJ,
-                full_output=True,
-                callback=custom_callback,
-                gtol=gtol
+            grad_J = np.zeros(n_coordinates, dtype=float)
+            grad_J_on_free_nodes = (
+                stiffness_matrix[free_nodes, :][:, free_nodes].dot(current_iterate[free_nodes])
+                +
+                load_vector_phi[free_nodes]
+                -
+                right_hand_side_vector[free_nodes]
             )
-            raise RuntimeError("fmin_cg failed to converge, stopping immediately")
-        except ConvergedException as conv:
-            current_iterate = conv.last_iterate
-            n_iterations = conv.n_iterations_done
+            grad_J[free_nodes] = grad_J_on_free_nodes
+            return grad_J
+
+        def J(current_iterate: np.ndarray) -> float:
+            J = (
+                0.5 * current_iterate.dot(stiffness_matrix.dot(current_iterate))
+                +
+                integrate_composition_nonlinear_with_fem(
+                    f=Phi,
+                    u=current_iterate,
+                    coordinates=coordinates,
+                    elements=elements,
+                    cubature_rule=CubatureRuleEnum.DAYTAYLOR)
+                -
+                right_hand_side_vector.dot(current_iterate)
+            )
+            return J
+
+        # Custom Stopping Criterion: Energy Tail-Off or Others
+        # ----------------------------------------------------
+        custom_callback = get_custom_callback(
+            stopping_criterion=args.stopping_criterion, args=args)
+
+        # distinguish two cases; default and non-default stopping criteria
+        # as default criteria does not implement a custom callback and therefore
+        # does not raise on convergence
+        if args.stopping_criterion == "default":
+            gtol = GTOL  # could be None, fmin_cg will use its default
+            current_iterate, f_opt, func_calls, grad_calls, _ = \
+                fmin_cg(
+                    f=J,
+                    x0=current_iterate,
+                    fprime=DJ,
+                    full_output=True,
+                    callback=custom_callback,
+                    gtol=gtol)
+            n_iterations = func_calls
+        else:
+            gtol = 1e-12  # or smaller to ensure custom stopping criterion is used
+            try:
+                _, _, _, _, _ = \
+                fmin_cg(
+                    f=J,
+                    x0=current_iterate,
+                    fprime=DJ,
+                    full_output=True,
+                    callback=custom_callback,
+                    gtol=gtol
+                )
+                raise RuntimeError("fmin_cg failed to converge, stopping immediately")
+            except ConvergedException as conv:
+                current_iterate = conv.last_iterate
+                n_iterations = conv.n_iterations_done
 
 
-    # nonlinear EVA
-    # -------------
-    _, edge_to_nodes, _ = \
-        provide_geometric_data(
-            elements=elements,
-            boundaries=boundaries)
+        # nonlinear EVA
+        # -------------
+        _, edge_to_nodes, _ = \
+            provide_geometric_data(
+                elements=elements,
+                boundaries=boundaries)
 
-    edge_to_nodes_flipped = np.column_stack(
-        [edge_to_nodes[:, 1], edge_to_nodes[:, 0]])
-    boundary = np.logical_or(
-        is_row_in(edge_to_nodes, boundaries[0]),
-        is_row_in(edge_to_nodes_flipped, boundaries[0])
-    )
-    non_boundary = np.logical_not(boundary)
-    edges = edge_to_nodes
-    non_boundary_edges = edge_to_nodes[non_boundary]
+        edge_to_nodes_flipped = np.column_stack(
+            [edge_to_nodes[:, 1], edge_to_nodes[:, 0]])
+        boundary = np.logical_or(
+            is_row_in(edge_to_nodes, boundaries[0]),
+            is_row_in(edge_to_nodes_flipped, boundaries[0])
+        )
+        non_boundary = np.logical_not(boundary)
+        edges = edge_to_nodes
+        non_boundary_edges = edge_to_nodes[non_boundary]
 
-    # free nodes / edges
-    n_vertices = coordinates.shape[0]
-    indices_of_free_nodes = np.setdiff1d(
-        ar1=np.arange(n_vertices),
-        ar2=np.unique(boundaries[0].flatten()))
-    free_nodes = np.zeros(n_vertices, dtype=bool)
-    free_nodes[indices_of_free_nodes] = 1
-    free_edges = non_boundary  # integer array (holding actual indices)
-    free_nodes = free_nodes  # boolean array
+        # free nodes / edges
+        n_vertices = coordinates.shape[0]
+        indices_of_free_nodes = np.setdiff1d(
+            ar1=np.arange(n_vertices),
+            ar2=np.unique(boundaries[0].flatten()))
+        free_nodes = np.zeros(n_vertices, dtype=bool)
+        free_nodes[indices_of_free_nodes] = 1
+        free_edges = non_boundary  # integer array (holding actual indices)
+        free_nodes = free_nodes  # boolean array
 
-    energy_gains = get_energy_gains_nonlinear(
-        coordinates=coordinates,
-        elements=elements,
-        non_boundary_edges=non_boundary_edges,
-        current_iterate=current_iterate,
-        f=f,
-        a_11=a_11,
-        a_12=a_12,
-        a_21=a_21,
-        a_22=a_22,
-        phi=phi,
-        phi_prime=phi_prime,
-        eta=ETA,
-        cubature_rule=CubatureRuleEnum.DAYTAYLOR,
-        verbose=True)
-
-    # dörfler based on EVA
-    marked_edges = np.zeros(edges.shape[0], dtype=int)
-    marked_non_boundary_egdes = doerfler_marking(
-        input=energy_gains, theta=THETA)
-    marked_edges[free_edges] = marked_non_boundary_egdes
-
-    element_to_edges, edge_to_nodes, boundaries_to_edges =\
-        provide_geometric_data(elements=elements, boundaries=boundaries)
-
-    coordinates, elements, boundaries, current_iterate = \
-        refineNVB_edge_based(
+        energy_gains = get_energy_gains_nonlinear(
             coordinates=coordinates,
             elements=elements,
-            boundary_conditions=boundaries,
-            element2edges=element_to_edges,
-            edge_to_nodes=edge_to_nodes,
-            boundaries_to_edges=boundaries_to_edges,
-            edge2newNode=marked_edges,
-            to_embed=current_iterate)
+            non_boundary_edges=non_boundary_edges,
+            current_iterate=current_iterate,
+            f=f,
+            a_11=a_11,
+            a_12=a_12,
+            a_21=a_21,
+            a_22=a_22,
+            phi=phi,
+            phi_prime=phi_prime,
+            eta=ETA,
+            cubature_rule=CubatureRuleEnum.DAYTAYLOR,
+            verbose=True)
+
+        # dörfler based on EVA
+        marked_edges = np.zeros(edges.shape[0], dtype=int)
+        marked_non_boundary_egdes = doerfler_marking(
+            input=energy_gains, theta=THETA)
+        marked_edges[free_edges] = marked_non_boundary_egdes
+
+        element_to_edges, edge_to_nodes, boundaries_to_edges =\
+            provide_geometric_data(elements=elements, boundaries=boundaries)
+
+        coordinates, elements, boundaries, current_iterate = \
+            refineNVB_edge_based(
+                coordinates=coordinates,
+                elements=elements,
+                boundary_conditions=boundaries,
+                element2edges=element_to_edges,
+                edge_to_nodes=edge_to_nodes,
+                boundaries_to_edges=boundaries_to_edges,
+                edge2newNode=marked_edges,
+                to_embed=current_iterate)
 
 
 if __name__ == '__main__':
