67835335da7ff536f4c03149903a1b6716e6342c
raphael leu <raphaelleu95@gmail.com>
2025-10-27 08:33:41 +0100

    status quo of some files

--- edge_based_variational_adaptivity/general_stiffness_matrix/perform_post_processing.sh
+++ edge_based_variational_adaptivity/general_stiffness_matrix/perform_post_processing.sh
@@ -8,22 +8,34 @@
 # bash calculate_all_errors.sh results/experiment_03 1 energy_norm_squared_reference_value_problem_01.dat
 # bash calculate_all_errors.sh results/experiment_04 2 energy_norm_squared_reference_value_problem_02.dat
 
-# bash calculate_all_errors.sh results/experiment_05 3 energy_norm_squared_reference_value_problem_03.dat
-# bash calculate_all_errors.sh results/experiment_06 3 energy_norm_squared_reference_value_problem_03.dat
+# bash calculate_all_errors.sh results/experiment_09 4 energy_norm_squared_reference_value_problem_04.dat
+# bash calculate_all_errors.sh results/experiment_10 4 energy_norm_squared_reference_value_problem_04.dat
 
-bash calculate_all_errors.sh results/experiment_07 3 energy_norm_squared_reference_value_problem_03.dat
-bash calculate_all_errors.sh results/experiment_08 3 energy_norm_squared_reference_value_problem_03.dat
+# bash calculate_all_errors.sh results/experiment_12 5 energy_norm_squared_reference_value_problem_05.dat
+# bash calculate_all_errors.sh results/experiment_13 5 energy_norm_squared_reference_value_problem_05.dat
+
+# Cobined Stopping Criteria
+# bash calculate_all_errors.sh results/experiment_14 1 energy_norm_squared_reference_value_problem_01.dat
+# bash calculate_all_errors.sh results/experiment_15 2 energy_norm_squared_reference_value_problem_02.dat
+# bash calculate_all_errors.sh results/experiment_16 4 energy_norm_squared_reference_value_problem_04.dat
+# bash calculate_all_errors.sh results/experiment_17 5 energy_norm_squared_reference_value_problem_05.dat
 
 # plotting
 # --------
-# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_01
-# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_02
+bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_01
+bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_02
 
-# bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_03
-# bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_04
+bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_03
+bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_04
 
-# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_05
-# bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_06
+bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_09
+bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_10
 
-# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_07
-# bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_08
+bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_12
+bash plot_all_errors.sh plot_energy_errors_energy_decay.py results/experiment_13
+
+# Combined Stopping Criteria
+# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_14
+# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_15
+# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_16
+# bash plot_all_errors.sh plot_energy_errors_arioli.py results/experiment_17
--- edge_based_variational_adaptivity/plain_convergence/plot_evidence_values.py
+++ edge_based_variational_adaptivity/plain_convergence/plot_evidence_values.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from pathlib import Path
 from load_save_dumps import load_dump
 import numpy as np
@@ -16,8 +17,11 @@ def main() -> None:
     energy_norm_squared_ref = 0.214075802220546
     energy_ref = -0.5*energy_norm_squared_ref
 
-    n_dofs = []
-    energy_differences = []
+    n_dofs: list[int] = []
+    energies: list[float] = []
+    energy_differences: list[float] = []
+    sums_energy_decays: list[float] = []
+    sums_marked_energy_decays: list[float] = []
     
     for n_dof_dir in base_path.iterdir():
         if not n_dof_dir.is_dir():
@@ -27,19 +31,55 @@ def main() -> None:
 
         path_to_energy_galerkin = n_dof_dir / Path('galerkin_solution_energy.pkl')
         energy_galerkin = load_dump(path_to_dump=path_to_energy_galerkin)
+        energies.append(energy_galerkin)
         energy_differences.append(energy_galerkin - energy_ref)
+
+        path_to_sum_energy_decays = n_dof_dir / Path('sum_energy_decays.pkl')
+        path_to_sum_marked_energy_decays = n_dof_dir / Path('sum_marked_energy_decays.pkl')
+
+        sum_marked_energy_decays = load_dump(path_to_dump=path_to_sum_marked_energy_decays)
+        sum_energy_decays = load_dump(path_to_dump=path_to_sum_energy_decays)
+
+        sums_energy_decays.append(sum_energy_decays)
+        sums_marked_energy_decays.append(sum_marked_energy_decays)
     
     n_dofs = np.array(n_dofs)
     energy_differences = np.array(energy_differences)
+    sums_energy_decays = np.array(sums_energy_decays)
+    sums_marked_energy_decays = np.array(sums_marked_energy_decays)
+    energies = np.array(energies)
 
     # sorting according to DOFs
     sort_n_dof = n_dofs.argsort()
     n_dofs = n_dofs[sort_n_dof]
     energy_differences = energy_differences[sort_n_dof]
+    sums_energy_decays = sums_energy_decays[sort_n_dof]
+    sums_marked_energy_decays = sums_marked_energy_decays[sort_n_dof]
+    energies = energies[sort_n_dof]
 
+    # extracting the relevant values
+    # ------------------------------
+
+    # (i) A posteriori error estimate
+    # E(u_H) - E(u) < C_1 sum_{S in S_H} dE_S
+    indicatori_i = energy_differences / sums_energy_decays
+    label_i = r'$\frac{E(u_H) - E(u)}{\sum_{S \in S_H} \Delta E_S}$'
+
+    # (ii) Lower bound on energy difference
+    # sum_{S in \tilde S_H} dE_S < C_2 (E(u_H) - E(u_h))
+    indicator_ii = sums_marked_energy_decays[:-1] / (energies[:-1] - energies[1:])
+    label_ii = r'$\frac{\sum_{S \in \tilde S_H \Delta E_S}}{E(u_H) - E(u_h)}$'
+
+    # (iii) Energy Contraction, i.e.
+    # E(u_h) - E(u) < q [E(u_H) - E(u)],  q in (0, 1)
     energy_quotients = energy_differences[1:] / energy_differences[:-1]
+    label_iii = r'$\frac{E(u_h) - E(u)}{E(u_H) - E(u)}$'
 
-    plt.semilogy(energy_quotients)
+    plt.semilogx(n_dofs[:-1], energy_quotients, label=label_iii)
+    plt.semilogx(n_dofs, indicatori_i, label=label_i)
+    plt.semilogx(n_dofs[:-1], indicator_ii, label=label_ii)
+    plt.hlines(y=1.0, xmin=n_dofs[2], xmax=n_dofs[-2], color='red')
+    plt.legend()
     plt.show()
 
 if __name__ == '__main__':
