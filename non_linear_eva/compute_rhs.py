1e98b5e1eb5f350657aa7841fdfbe61827512207
raphael leu <raphaelleu95@gmail.com>
2025-10-29 10:57:36 +0100

    chore: implemented problem 4

--- non_linear_eva/compute_rhs.py
+++ non_linear_eva/compute_rhs.py
@@ -2,13 +2,14 @@ import sympy as sp
 from sympy.utilities.lambdify import lambdastr
 
 
-def print_rhs_problem_01():
+def print_rhs_problem_04():
     x, y = sp.symbols('x y')
 
     # Problem configuration
     # ---------------------
     # solution
-    u = x*(x-1)*y*(y-1)
+    r = sp.sqrt(x*x + y*y)
+    u = 2. * r**(-4/3) * x * y * (1 - x*x) * (1 - y*y)
     # diffusion matrix
     a_11 = 1.
     a_12 = 0.
@@ -16,7 +17,7 @@ def print_rhs_problem_01():
     a_22 = 1.
     A = sp.Matrix([[a_11, a_12], [a_21, a_22]])
     # reaction term
-    phi = sp.exp(u)
+    phi = u**3
     # ---------------------
 
     # gradients and divergences
@@ -28,13 +29,14 @@ def print_rhs_problem_01():
     f = -div_A_grad_u + phi
 
     # printing f as string
-    print('Problem 01')
+    print('Problem 04')
     print('----------')
+    print(f.simplify())
     print(lambdastr((x,y), f.simplify()))
 
 
 def main() -> None:
-    print_rhs_problem_01()
+    print_rhs_problem_04()
 
 
 if __name__ == '__main__':
--- non_linear_eva/problems.py
+++ non_linear_eva/problems.py
@@ -217,6 +217,63 @@ def get_problem_3() -> Problem:
         get_coarse_initial_mesh=get_coarse_L_shape_mesh)
 
 
+def get_problem_4() -> Problem:
+    def f(r: CoordinatesType) -> float:
+        """
+        returns the RHS function f of the PDE
+        - laplace u(x, y) + u(x, y)^3 = f(x, y),
+        if we impose
+        u(x, y) = 2r^{-4/3}xy(1-x^2)(1-y^2),
+        where r:= sqrt(x^2 + y^2),
+        see [1, chapter 4.2].
+
+        References
+        ----------
+        [1] https://arxiv.org/abs/2504.11292
+
+        Notes
+        -----
+        the right hand side was computed
+        (and left unchanged, that's why it looks horrible)
+        using the accompanying script
+        `compute_rhs.py`
+        in this folder
+        """
+        x, y = r[:, 0], r[:, 0]
+        return (x*y*(x**2 + y**2)**(-7.0)*(8.0*x**2*y**2*(x**2 - 1)**3*(x**2 + y**2)**5.0*(y**2 - 1)**3 - 8.88888888888889*(x**2 - 1)*(x**2 + y**2)**5.33333333333333*(y**2 - 1) + (x**2 + y**2)**5.33333333333333*(10.6666666666667*x**2*(y**2 - 1) + 10.6666666666667*y**2*(x**2 - 1) + 16.0*(x**2 - 1)*(y**2 - 1)) + 12.0*(x**2 + y**2)**6.33333333333333*(-x**2 - y**2 + 2)))
+
+    def a_11(r: CoordinatesType) -> np.ndarray:
+        n_vertices = r.shape[0]
+        return - np.ones(n_vertices, dtype=float)
+
+    def a_22(r: CoordinatesType) -> np.ndarray:
+        n_vertices = r.shape[0]
+        return - np.ones(n_vertices, dtype=float)
+
+    def a_12(r: CoordinatesType) -> np.ndarray:
+        n_vertices = r.shape[0]
+        return np.zeros(n_vertices, dtype=float)
+
+    def a_21(r: CoordinatesType) -> np.ndarray:
+        n_vertices = r.shape[0]
+        return np.zeros(n_vertices, dtype=float)
+
+    def phi(u: np.ndarray) -> np.ndarray:
+        return u**3
+
+    def Phi(u: np.ndarray) -> np.ndarray:
+        return u**4. / 4.
+    
+    def phi_prime(u: np.ndarray) -> np.ndarray:
+        return 3. * u**2.
+
+    return Problem(
+        f=f, a_11=a_11, a_12=a_12,
+        a_21=a_21, a_22=a_22,
+        phi=phi, phi_prime=phi_prime, Phi=Phi,
+        get_coarse_initial_mesh=get_coarse_L_shape_mesh)
+
+
 def get_problem(number: int) -> Problem:
     if number == 1:
         return get_problem_1()
